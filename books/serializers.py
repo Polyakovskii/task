1177309de7e2cf7ebe65ceef06956d77f2290890
Polyakovskii <a.polyakovskii@gmail.com>
2020-07-16 12:53:06 +0300

    "Description" problem resolved.

--- books/serializers.py
+++ books/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 from .models import Book
 
-class BookSerializer(serializers.ModelSerializer):
+class SingleBookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'author_name', 'description')
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'author_name')
--- books/views.py
+++ books/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, RetrieveAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializer, SingleBookSerializer
 # Create your views here.
 
-class BookView(ListModelMixin, CreateModelMixin, GenericAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class BookView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response({"books": serializer.data})
+    def post(self, request):
+        book = request.data.get('book')
+        # Create an article from the above data
+        serializer = SingleBookSerializer(data=book)
+        if serializer.is_valid(raise_exception=True):
+            book_saved = serializer.save()
+        return Response({"success": "Book '{}' created successfully".format(book_saved.title)})
 
 class SingleBookView(RetrieveAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = SingleBookSerializer
