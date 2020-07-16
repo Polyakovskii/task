c2a09997a4f4dd067eefd092e48e86073ef7dcd0
Polyakovskii <a.polyakovskii@gmail.com>
2020-07-16 18:29:19 +0300

    Filters have been added.

--- books/views.py
+++ books/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
-
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
+from .filters import BookFilter
 from .serializers import BookSerializer, SingleBookSerializer
 
 class BookView(viewsets.GenericViewSet, viewsets.mixins.RetrieveModelMixin, viewsets.mixins.CreateModelMixin, viewsets.mixins.ListModelMixin):
     queryset = Book.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = BookFilter
     def get_serializer_class(self):
         if self.action == 'list':
             return BookSerializer
